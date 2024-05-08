8d6fbcc2a9200d219bee4823fbb517ef560d8e8e
Chesterkxng <cgoita00@gmail.com>
2024-05-08 20:01:40 +0000

    init all and task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create a table usrs if it doesn't exist
+-- if exist no fail
+CREATE TABLE IF NOT EXISTS users (
+       id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
+);
