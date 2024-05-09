ab76be678af8e5a42a3fb971929a9c23abb28588
Chesterkxng <cgoita00@gmail.com>
2024-05-09 23:21:51 +0000

    inf

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,5 +3,5 @@ CREATE VIEW need_meeting
 AS
     SELECT name
     FROM students
-    WHERE score < 80 AND last_meeting IS NULL OR last_meeting > DATE_SUB(NOW(), INTERVAL
+    WHERE score < 80 AND last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL
 1 MONTH);
