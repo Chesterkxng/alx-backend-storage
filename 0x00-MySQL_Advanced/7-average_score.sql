aa17dfbd78d6a610269ffb6f52398ae39f096f00
Chesterkxng <cgoita00@gmail.com>
2024-05-09 20:23:30 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,10 @@
 -- stored procedure ComputeAverageScoreForUser that
 -- computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average FLOAT;
+    DECLARE average FLOAT DEFAULT 0;
 
     SELECT AVG(score) INTO average
     FROM corrections
