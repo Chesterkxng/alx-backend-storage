bbe3de466921ffed3f2791e2c65e5af356c84b7e
Chesterkxng <cgoita00@gmail.com>
2024-05-09 20:41:36 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,14 +4,9 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average FLOAT DEFAULT 0;
-
-    SELECT AVG(score) INTO average
-    FROM corrections
-    WHERE user_id = user_id;
-
-    UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
+    	UPDATE users
+	SET average_score = (SELECT SUM(score) / COUNT(*) FROM corrections
+	 WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 END$$
 DELIMITER ;
