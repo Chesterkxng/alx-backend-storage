793cf118b6c9642420c546bbdb374e47dcbad03e
Chesterkxng <cgoita00@gmail.com>
2024-05-16 12:25:51 +0000

    NFM

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''request, cache and track.
+'''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of result.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''return URL after adding cache.
+    '''
+    return requests.get(url).text
